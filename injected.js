ca2d3e4d6e414ea0cbf3be13a0d86a02681d846c
zeldoon <20314483+zeldoon@users.noreply.github.com>
2025-10-02 11:24:38 -0400

    Update injected.js
    fixes breaks

--- injected.js
+++ injected.js
@@ -1,37 +1,41 @@
+console.log("injected.js loaded")
+
 const originalFetch = window.fetch;
 
-// Override the window.fetch with our custom function
-// This is so we can grab the URL of the raw gltf file
-
 window.fetch = async function (...args) {
+  const [input, init] = args;
 
-  const [url, options] = args;
+  let requestUrl = "";
+  let method = "GET";
 
-
-  if (url.method == "GET"){
-    if (url.url.startsWith("blob:https://app.shapr3d.com/")){
-      
-      //found the blob url
-      window.MODEL_GLTF_URL = url.url
-      stl_ready()
-      
-    }
+  if (typeof input === "string") {
+    requestUrl = input;
+  } else if (input instanceof Request) {
+    requestUrl = input.url;
+    method = input.method;
   }
 
+  if (init && init.method) {
+    method = init.method;
+  }
 
-  // 4) Call the original fetch
-  const response = await originalFetch(...args);
+  // Detect the blob URL we care about
+  if (method === "GET" && requestUrl.startsWith("blob:https://app.shapr3d.com/")) {
+    console.log("Found blob URL:", requestUrl);
+    window.MODEL_GLTF_URL = requestUrl;
+    stl_ready();
+  }
 
-  return response;
+  return originalFetch.apply(this, args);
 };
 
-
-
 window.stl_ready = async function(){
+    
 
-    var url = "https://gist.githubusercontent.com/zeldoon/5ef17d73cedc6fa15da0f8dfd0e9ee01/raw/3fe735d6fcbb4e0d8c958979208233d961e5a3b1/stlmaker.js"
+    var url = "https://static.candela.io/three/stlmaker.js"
 
     const script = document.createElement('script');
+    console.log("Loading STL maker script");
     script.src = url;
     script.type = "module"
     document.head.appendChild(script);
